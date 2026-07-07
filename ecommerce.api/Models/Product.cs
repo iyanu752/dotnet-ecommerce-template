f00ac423de8b97708fa2a3e5f6c0d99a7b002aa5
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2026-07-07 13:54:23 +0100

    [feat]: added entitiy framework and implemented postgresql database

--- ecommerce.api/Models/Product.cs
+++ ecommerce.api/Models/Product.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ecommerce.api;
 
@@ -7,6 +8,7 @@ public class Product
     public int Id {get; set;}
     public string Name { get; set;} = string.Empty;
     public string Description { get; set;} = string.Empty;
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Price {get; set;}
     public int Stock {get; set;}
     public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
--- ecommerce.api/Program.cs
+++ ecommerce.api/Program.cs
@@ -1,3 +1,6 @@
+using ecommerce.api;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
